0f79c266df34799034efc3765ebc377e4d9f93b0
Kendra Wing <kendracwing@icloud.com>
2022-10-03 15:43:09 -0600

    merge updates from dev to main branch (#45)
    * Removed redundant files.
    
    Removed images included with create-react.
    
    * Installed npm package
    
     "react-page-scroller": "^2.2.1"
    
    * Updated files
    
    Updated/edited files...
    
    * Added note to ```README.md```
    
    * 📝Added info to ```README.md```
    
    Updated info for project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,110 @@
 //import './App.css';
-import "./App.scss"
+import React from 'react';
+
+import "./App.scss";
 
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import MainContent from "./pages/Main";
+import About from "./pages/About";
+
+import ReactPageScroller from 'react-page-scroller';
 
 
-export default function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Home />
-      <MainContent />
-    </div>
-  );
+
+// export default function App() {
+//   constructor(props) {
+//     super(props);
+//     this.state = { currentPage: null };
+//   }
+
+//   handlePageChange = number => {
+//     this.setState({ currentPage: number });
+//   };
+
+//   handleBeforePageChange = number => {
+//     console.log(number);
+//   };
+
+//   getPagesNumbers = () => {
+//     const pageNumbers = [];
+
+//     for (let i = 1; i <= 5; i++) {
+//       pageNumbers.push(
+//         <Pager.Item key={i} eventKey={i - 1} onSelect={this.handlePageChange}>
+//           {i}
+//         </Pager.Item>,
+//       );
+//     }
+
+//     return [...pageNumbers];
+//   };
+
+//   render() {
+//     const pagesNumbers = this.getPagesNumbers();
+
+
+//   return (
+//     <div className="App">
+//       <React.Fragment>
+//         <ReactPageScroller
+//           pageOnChange={this.handlePageChange}
+//           onBeforePageScroll={this.handleBeforePageChange}
+//           customPageNumber={this.state.currentPage}
+//         >
+//       <Nav />
+//       <Home />
+//       <MainContent />
+//       </ReactPageScroller>
+//       </React.Fragment>
+//     </div>
+//   );
+//   }
+// }
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Nav />
+//       <Home />
+//       <MainContent />
+//     </div>
+//   );
+// }
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: null };
+  }
+
+  handlePageChange = number => {
+    this.setState({ currentPage: number });
+  };
+
+  handleBeforePageChange = number => {
+    console.log(number);
+  };
+
+
+
+  render() {
+
+
+    return (
+      <React.Fragment>
+        <ReactPageScroller
+          pageOnChange={this.handlePageChange}
+          onBeforePageScroll={this.handleBeforePageChange}
+          customPageNumber={this.state.currentPage}
+        >
+          <Nav />
+          <Home />
+          <MainContent />
+          <About />
+        </ReactPageScroller>
+
+      </React.Fragment>
+    );
+  }
 }
